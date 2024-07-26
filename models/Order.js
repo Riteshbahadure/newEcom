4ec5192d24bf66ee2415ad3e1108849573438cf6
Rutuja Bahadure <bahadurerutu558@gamil.com>
2024-07-26 23:19:07 +0530

    ofngkdfdk

--- models/Order.js
+++ models/Order.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
     customer: { type: mongoose.Types.ObjectId, ref: "user", required: true },
@@ -6,6 +6,8 @@ const orderSchema = new mongoose.Schema({
     name: { type: String, required: true },
     address: { type: String, required: true },
     mobile: { type: String, required: true },
-    status: { type: String, enum: ["placed", "delivered", "cancel",], default: "placed" }
-})
-module.exports = mongoose.model("order", orderSchema)
+    status: { type: String, enum: ["placed", "delivered", "cancel"], default: "placed" },
+    // otp: { type: String, required: true } // Add this field
+});
+
+module.exports = mongoose.model("order", orderSchema);
